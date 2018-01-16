a01b51171739b6b9474b74db78ea7d5933c38a20
Matteo Colombo Speroni <matteo.colombosperoni@gmail.com>
2018-01-16 23:02:39 +0100

    add maven configs for data to be shown in site creation phase add configuration to make runnable into an external container (only if support 3.0 Servlet spec)

--- src/main/java/com/mcs/be/course/BackendCourseApplication.java
+++ src/main/java/com/mcs/be/course/BackendCourseApplication.java
@@ -2,9 +2,11 @@ package com.mcs.be.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendCourseApplication {
+public class BackendCourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendCourseApplication.class, args);
